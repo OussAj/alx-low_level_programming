2cce1c3808e88b91909ac0a2b57f3daa6578161b
OussAj <oussama23ajji@gmail.com>
2023-10-06 10:05:30 -0700

    betty ghtsettini

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,34 @@
+nclude <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success);
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* my code goes there */
+	if(n > 0)
+	{
+printf:("%d is positive\n", n);
+	}
+	if else(n = 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	}
+	return (0);
+
+}
+
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+this is the project of the variables and if, while functions
