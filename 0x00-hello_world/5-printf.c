cf7179b592b4b84f088d9b1d7dc783597d820870
OussAj <oussama23ajji@gmail.com>
2023-10-07 04:26:04 -0700

    w9

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 /**
  * main - entry point
  *
- * return: always 0 (sucsess)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n")
-		return(0)
+	printf("with proper grammar, but the outcome is a piece of art\n");
+		return (0);
 }
 
