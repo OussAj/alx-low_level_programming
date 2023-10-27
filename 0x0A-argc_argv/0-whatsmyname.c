d702b516f37086118af85094f68b17c54554169f
OussAJ <oussama23ajji@gmail.com>
2023-10-27 10:36:29 -0700

    bsmlh2

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name.
+ *
+ * @argc: parameter.
+ * @argv: parameter.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
