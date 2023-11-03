2f1066327aca07367101eba8a02d5c786291e07f
OussAJ <oussama23ajji@gmail.com>
2023-11-03 14:27:31 -0700

    inchallah

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,13 +11,13 @@
  */
 int *array_range(int min, int max)
 {
-	int *s, i = 0, n, j = 0;
+	int *s, i = 0, n;
 
 	if (min > max)
 	{
 		return (NULL);
 	}
-	n = max - min;
+	n = ((max - min) + 1);
 	s = malloc(sizeof(int) * n);
 	if (s == NULL)
 	{
@@ -25,4 +25,9 @@ int *array_range(int min, int max)
 	}
 	while (i <= n)
 	{
+		s[i] = (min + i);
+		i++;
+	}
+	return (s);
+}
 
