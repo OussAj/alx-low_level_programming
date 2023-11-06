86f5c7d1c2b6db9e0b481f9b376fd0940ab06e9f
OussAJ <oussama23ajji@gmail.com>
2023-11-06 13:03:03 -0800

    just do it

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,59 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+char *_strdup(char *s);
+/**
+ * *new_dog - function that creates a new dog
+ * @name: parameter
+ * @age: parameter
+ * @owner: parameter
+ * Return: result
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new;
+
+	new = (dog_t *) malloc(sizeof(dog_t));
+
+	if (!new)
+	{
+		return (NULL);
+	}
+	(*new).name = _strdup(name);
+
+	if (!(*new).name)
+	{
+		free(new);
+		return (NULL);
+	}
+	(*new).age = age;
+	(*new).owner = _strdup(owner);
+
+	if (!(*new).owner)
+	{
+		free((*new).name);
+		free(new);
+		return (NULL);
+	}
+	return (new);
+}
+/**
+ * *_strdup - function that duplicates a string
+   @s: paremeter
+ *  Return: result
+ */
+char *_strdup(char *s)
+{
+	size_t len = strlen(s) + 1;
+	char *copy = malloc(len);
+
+	if (!copy)
+	{
+		return (NULL);
+	}
+	memcpy(copy, s, len);
+	return (copy);
+}
+
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - a function that frees dogs.
+ * @d: parameter.
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+		{
+			free(d->name);
+		}
+		if (d->owner)
+		{
+			free(d->owner);
+		}
+		free(d);
+	}
+}
+
